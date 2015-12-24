f445b21390137df5b23125a8bf3ef97bec1c44bc
Luke Makai <lmmakai@gmail.com>
2015-12-24 16:28:35 -0500

    first commit

---
+++ source/content/pages/finance.markdown
@@ -0,0 +1,9 @@
+title: Finance
+category: page
+slug: finance
+meta: Finance meta description for page.
+
+
+# Finance
+Finance is an evil witchcraft that takes money from the feeble and gives
+it to bros with popped collars.
---
+++ source/settings.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+AUTHOR = u'Luke Makai'
+SITENAME = u'Risk Developer'
+SITEURL = 'http://www.riskdeveloper.com'
+TIMEZONE = 'America/New_York'
+
+GITHUB_URL = 'https://github.com/lmmakai/riskdeveloper.com'
+DISQUS_SITENAME = ''
+PDF_GENERATOR = False
+
+DIRECT_TEMPLATES = ('index', 'sitemap')
+
+SITEMAP_SAVE_AS = 'sitemap.xml'
+
+BYLINE = '&copy; 2016 Luke Makai. All Rights Reserved.'
+LINKS = ()
+
+MARKUP = ('rst', 'markdown',)
+
+SOCIAL = (
+    ('Email', 'mailto:lmmakai@gmail.com'),
+    ('GitHub', 'https://github.com/lmmakai'),
+    ('Twitter', 'http://twitter.com/riskdeveloper'),
+)
+
+PROJECTS = ()
+
+JINJA_EXTENSIONS = (['jinja2.ext.autoescape',])
+
